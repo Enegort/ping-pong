8bf81ac564aa8aa1587e6c7e4f7fa666eab6df10
Enegort <124575767+Enegort@users.noreply.github.com>
2026-06-06 10:32:00 -0700

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -1,224 +1,105 @@
 from pygame import *
-from random import randint
-from time import time as timer
+'''Required classes'''
 
-#música
-mixer.init()
-mixer.music.load('space.ogg')
-mixer.music.play()
-fire_sound = mixer.Sound('fire.ogg')
+
+#clase padre para los objetos
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height)) #por ejemplo. 55,55 - parámetros
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+   def update_r(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+def update_l(self):
+       keys = key.get_pressed()
+       if keys[K_w] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+
+
+#escena del videojuego:
+back = (200, 255, 255) #цвет фона (fondo)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+
+#banderas responsables por el estado del juego
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+
+#creando pelota y paletas 
+racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
 
 font.init()
-font1 = font.Font(None,80)
-win = font1.render('Ganaste.', True, (50, 255, 50))
-lose = font1.render('Perdiste!', True, (180, 0, 0))
-
-font2 = font.Font(None, 36)
-
-img_back = 'galaxy.jpg'
-img_hero = 'rocket.png'
-img_bullet = 'bullet.png'
-img_enemy = 'ufo.png'
-img_asteroide = 'Asteroide.png'
-
-score = 0
-goal = 10
-lost = 0
-max_lost = 3
-life = 3
-
-# clase padre para otros objetos
-class GameSprite(sprite.Sprite):
-    # constructor de clase
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        # llamamos al constructor de la clase (Sprite):
-        sprite.Sprite.__init__(self)
-
-        # cada objeto debe almacenar una propiedad image
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
-        self.speed = player_speed
-
-        # cada objeto debe almacenar la propiedad rect en la cual está inscrito
-        self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
-
-    # método que dibuja al personaje en la ventana
-    def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
-
-# clase del jugador principal
-class Player(GameSprite):
-    def update(self):
-        keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-
-    def fire(self):
-        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
-        bullets.add(bullet)
-
-class Enemy(GameSprite):
-    def update(self):
-        self.rect.y += self.speed
-        global lost
-        #Desaparecer
-        if self.rect.y > win_height:
-            self.rect.x = randint(80, win_width - 80)
-            self.rect.y = 0
-            lost = lost + 1
-
-class Bullet(GameSprite):
-    def update(self):
-        self.rect.y += self.speed
-        if self.rect.y < 0:
-            self.kill
-
-#Ventana
-win_width = 700
-win_height = 500
-display.set_caption('Tirador')
-window = display.set_mode((win_width, win_height))
-background = transform.scale(image.load(img_back), (win_width, win_height))
-
-#crea objetos
-ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
-
-monsters = sprite.Group()
-for i in range(1, 6):
-    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-    monsters.add(monster)
-
-asteroids = sprite.Group()
-for i in range(1, 3):
-    asteroid = Enemy(img_asteroide, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-    asteroids.add(asteroid)
-
-bullets = sprite.Group()
-#la variable 'el juego termino': cuando sea True, los objetos dejan the funcionar en el ciclo principal
-    
-#ciclo de juego
-run = True
-finish = False 
-
-rel_time = False
-
-num_fire = 0
-
-while run:
-
-    for e in event.get():
-        if e.type == QUIT:
-            run = False
-
-        if e.type  == MOUSEBUTTONDOWN:
-            if num_fire < 5 and rel_time == False:
-                num_fire += + 1
-                fire_sound.play()
-                ship.fire()
-
-            if num_fire >= 5 and rel_time == False:
-                last_time = timer()
-                rel_time = True
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+speed_x = 3
+speed_y = 3
 
 
-        elif e.type == KEYDOWN:
-            if e.key == K_SPACE:
-                if num_fire < 5 and rel_time == False:
-                    num_fire += + 1
-                    fire_sound.play()
-                    ship.fire()
+while game:
+   for e in event.get():
+       if e.type == QUIT:
+           game = False
+  
+   if finish != True:
+       window.fill(back)
+       racket1.update_l()
+       racket2.update_r()
+       ball.rect.x += speed_x
+       ball.rect.y += speed_y
 
-                if num_fire >= 5 and rel_time == False:
-                    last_time = timer()
-                    rel_time = True
 
-    if not finish:
-        #actualiza el fondo
-        window.blit(background,(0, 0))
+       if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+           speed_x *= -1
+           speed_y *= 1
+#si la pelota llega a los bordes de la pantalla, cambiar dirección de movimiento
+       if ball.rect.y > win_height-50 or ball.rect.y < 0:
+           speed_y *= -1
 
-        text = font2.render('Puntaje: ' + str(score), 1, (255, 255, 255))
-        window.blit(text,(10, 20))
 
-        text_lose = font2.render('Fallos: ' + str(lost), 1, (255, 255, 255))
-        window.blit(text_lose,(10, 50))
-    
-        #ejecuta los movimientos del objeto
-        ship.update()
-        monsters.update()
-        asteroids.update()
-        bullets.update()
+       #si la pelota va más allá de esta paleta, mostrar la condición de derrota para el jugador 1
+       if ball.rect.x < 0:
+           finish = True
+           window.blit(lose1, (200, 200))
+           game_over = True
 
-        #los actualiza en una nueva ubicacion en cada iteracion del ciclo
-        ship.reset()
-        monsters.draw(window)
-        asteroids.draw(window)
-        bullets.draw(window)
 
-        if rel_time == True:
-            now_time = timer()
+       #si la pelota va más allá de esta paleta, mostrar la condición de derrota para el jugador 2
+if ball.rect.x > win_width:
+           finish = True
+           window.blit(lose2, (200, 200))
+           game_over = True
 
-            if now_time - last_time < 3:
-                reload = font2.render('Reloading...', 1, (150, 0, 0))
-                window.blit(reload, (260, 240))
-            else:    
-                num_fire = 0 
-                rel_time = False
 
-        collides = sprite.groupcollide(monsters, bullets, True, True)
-        if len(collides) > 0:
-            for c in collides:
-                score += 1
-                monster = Enemy(img_enemy, randint(80, win_width - 80) -40, 80, 50, randint(1,5), 2)
+       racket1.reset()
+       racket2.reset()
+       ball.reset()
 
-                monsters.add(monster)
 
-        if sprite.spritecollide(ship, monsters, False) or sprite.spritecollide(ship, asteroids, False):
-            sprite.spritecollide(ship,monsters, True)
-            sprite.spritecollide(ship, monsters, True)
-            sprite.spritecollide(ship, asteroids, True)
-            life = life - 1
-
-        if sprite.spritecollide(ship, monsters, False) or lost >=  max_lost:
-            finish = True
-            window.blit(lose, (200, 200))
-            
-        if score >= goal:
-                    #establecer un color diferente dependiendo del número de vidas
-            if life == 3:
-                life_color = (0, 150, 0)
-            if life == 2:
-                life_color = (150, 150, 0)
-            if life == 1:
-                life_color = (150, 0, 0)
-        
-            text_life = font1.render(str(life), 1, life_color)
-            window.blit(text_life, (650, 10))
-        display.update()
-
-    else:
-        finish = False
-        score = 0
-        lost = 0
-        lost = 0
-        num_fire = 0
-        life = 3
-        for b in bullets:
-            b.kill()
-        for m in monsters:
-            m.kill()
-        for a in asteroids:
-            a.kill()
-
-        time.delay(3000)
-        for i in range(1,6):
-            monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-
-        for i in range(1, 3):
-            asteroid = Enemy(img_asteroide, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-            asteroids.add(asteroid)
-    
-    #el ciclo se ejecuta cada 0.03 seg
-    time.delay(30)
+   display.update()
+   clock.tick(FPS)
